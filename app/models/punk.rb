813f50113372f35a20ec613a20721f50ab683ccc
Kumar Akarsh <akarsh1357@gmail.com>
2019-09-29 13:38:28 +0200

    Add methods for punk sale information

--- app/models/punk.rb
+++ app/models/punk.rb
@@ -8,4 +8,17 @@ class Punk < ApplicationRecord
   has_and_belongs_to_many :accessories
   has_one :punk_listing
 
+  # Returns list of punks available for sale
+  scope :sale_listings, -> { joins(:punk_listing).where('punk_listings.for_sale =?', true) }
+
+  # Determines if punk is available for sale or not
+  def for_sale
+    punk_listing&.for_sale || false
+  end
+
+  # Returns minimum for sale price if punk is available for sale, 0 otherwise
+  def sale_price
+    return 0 unless for_sale
+    punk_listing&.sale_price || 0
+  end
 end
--- spec/models/punk_spec.rb
+++ spec/models/punk_spec.rb
@@ -5,4 +5,42 @@ require 'rails_helper'
 RSpec.describe Punk, type: :model do
   it { is_expected.to have_and_belong_to_many(:accessories) }
   it { is_expected.to have_one(:punk_listing) }
+
+  describe '#for_sale' do
+    let(:punk) { create(:punk) }
+    subject { punk.for_sale }
+
+    context 'when punk has no listing' do
+      it { is_expected.to(eq(false)) }
+    end
+
+    context 'when punk has listing present but is not available for sale' do
+      before { create(:punk_listing, punk_id: punk.id, for_sale: false, sale_price: '100000') }
+      it { is_expected.to(eq(false)) }
+    end
+
+    context 'when punk has listing present and is available for sale' do
+      before { create(:punk_listing, punk_id: punk.id, for_sale: true, sale_price: '100000') }
+      it { is_expected.to(eq(true)) }
+    end
+  end
+
+  describe '#sale_price' do
+    let(:punk) { create(:punk) }
+    subject { punk.sale_price }
+
+    context 'when punk has no listing' do
+      it { is_expected.to(eq(0)) }
+    end
+
+    context 'when punk has listing present but is not available for sale' do
+      before { create(:punk_listing, punk_id: punk.id, for_sale: false, sale_price: '100000') }
+      it { is_expected.to(eq(0)) }
+    end
+
+    context 'when punk has listing present and is available for sale' do
+      before { create(:punk_listing, punk_id: punk.id, for_sale: true, sale_price: '100000') }
+      it { is_expected.to(eq('100000')) }
+    end
+  end
 end
